07a1f15afaa1780bfbadd21993d479ee665122bb
FCX <2814773144@qq.com>
2019-05-05 02:16:10 +0800

    使用矩求中点

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <opencv2/imgproc/imgproc.hpp>
 
 int main(int argc,char** argv) {
-    std::string video = "1.mp4";
+    std::string video = "2.avi";
     if(argc != 1)
         video = argv[1];
     std::cout << "Hello, World!" << std::endl;
@@ -11,7 +11,7 @@ int main(int argc,char** argv) {
     capture.open(video);
     if(!capture.isOpened())
         std::cout<<"No such a video!"<<std::endl;
-    cv::Mat frame;
+    cv::Mat frame,gray;
     cv::namedWindow("test",cv::WINDOW_AUTOSIZE);
 
     while(1)
@@ -22,7 +22,20 @@ int main(int argc,char** argv) {
             std::cout<<"end"<<std::endl;
             break;
         }
-        cv::imshow("test",frame);
+        cv::Canny(frame,gray,256,256);
+        cv::Mat out(gray.rows,gray.cols,CV_8UC3,cv::Scalar(0,0,0));
+        std::vector<std::vector<cv::Point>> contours;
+        cv::findContours(gray,contours,cv::RETR_EXTERNAL,cv::CHAIN_APPROX_TC89_KCOS);
+        cv::drawContours(out,contours,-1,cv::Scalar(255,0,255),2,cv::LINE_AA);
+        std::vector<cv::Moments> dian(contours.size());
+        for(int i=0;i<contours.size();i++)
+            dian[i]=cv::moments(contours[i],1);
+        std::vector<cv::Point2f> mc(contours.size());
+        for(int i=0;i<contours.size();i++)
+            mc[i]=cv::Point2f(dian[i].m10/dian[i].m00,dian[i].m01/dian[i].m00);
+        for(int i=0;i<mc.size();i++)
+            cv::circle(out,mc[i],3,cv::Scalar(255,0,255),1,cv::LINE_AA);
+        cv::imshow("test",out);
         if(cv::waitKey(33) == 27) break;
     }
 
